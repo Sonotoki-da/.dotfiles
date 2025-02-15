de7790c34471168748edc5ac0fafd84120369286
Sonotoki-da <nonameuserbox02@gmail.com>
2025-02-15 17:17:10 +0500

    Endeavour sway commit

--- zsh/.oh-my-zsh/custom/sohro_configs.zsh
+++ zsh/.oh-my-zsh/custom/sohro_configs.zsh
@@ -1,5 +1,3 @@
-export ZDOTDIR=~/.config/zsh
-
 export PATH=$PATH:~/.local/scripts
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
@@ -12,3 +10,4 @@ alias zigdev="~/.zig/zig"
 md2docx() {
     pandoc -t latex "$1" | pandoc -f latex --data-dir=docs/rendering/ -o "${2:-$(basename "$1" .md).docx}"
 }
+
